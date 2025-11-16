196db259a8adf014e163d183161801e71b27f2aa
JT <jstorbit@gmail.com>
2025-11-15 23:41:44 -0700

    Update selector to target id and call render

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 const render = () => {
-  const $app = document.querySelector(`app`);
+  const $app = document.querySelector(`#app`);
   $app.innerHTML = `
   <h1>Odds and Events</h1>
   <p>Add a number to the bank</p>
@@ -9,5 +9,8 @@ const render = () => {
   <h2>Odds</h2>
   <OddNumbers></OddNumbers>
   <h2>Evens</h2>
-  <EvenNumbers></EvenNumbers>`
+  <EvenNumbers></EvenNumbers>`;
+
 }
+
+render();
